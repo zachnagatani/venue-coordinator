d050481cf891e10f03be2668ce83a4f224be5688
Zachariah Nagatani <zachnagatani@gmail.com>
2017-02-28 09:00:20 -0800

    Feat: User Model Module

--- server/auth/model/user.js
+++ server/auth/model/user.js
@@ -1,6 +1,21 @@
-const Schema = require('mongoose').Schema,
+const mongoose = require('mongoose'),
+      Schema = require('mongoose').Schema,
       userSchema = new Schema({
           username: {type: String, required: true, index: {unique: true}},
           email: {type: String, required: true, index: {unique: true}},
           password: {type: String, required: true}
-      });
+      }),
+      SECRET = process.env.SECRET,
+      jwt = require('jsonwebtoken');
+
+userSchema.methods.generateJwt = (username, id) => {
+    const expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7);
+    return jwt.sign({
+        _id: id,
+        username: username,
+        exp: parseInt(expiry.getTime() / 1000)
+    }, secret);
+};
+
+module.exports = mongoose.model('User', userSchema);
